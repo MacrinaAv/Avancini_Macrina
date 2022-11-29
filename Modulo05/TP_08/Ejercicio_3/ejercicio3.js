3e53dcf2e6f145c7c048ac7ca827eeee6e0efcae
MacrinaAv <avanciniluz@gmail.com>
2022-11-29 12:31:12 -0300

    subo el ejercicio 3 resuelto

---
+++ Modulo05/TP_08/Ejercicio_3/ejercicio3.js
@@ -0,0 +1,8 @@
+'use strict';
+const fs = require('fs');
+fs.readFile('C:/Users/avanc/Avancini_Macrina/Modulo05/TP_07/personas.json', (err, data) => {
+if (err) throw err;
+console.log(JSON.stringify(data));//a) interpreta los datos buffer sin procesar como JavaScript y los devuelve como JSON en JSON
+console.log(data);//b)se ven los datos buffer sin procesar 
+});
+
