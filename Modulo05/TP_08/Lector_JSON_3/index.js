66f55b5cbe3c2bc4e07e51e4c4bc8024e03aef40
MacrinaAv <avanciniluz@gmail.com>
2022-11-29 12:27:58 -0300

    correscciones fe ejemplos

--- Modulo05/TP_08/Lector_JSON_3/index.js
+++ Modulo05/TP_08/Lector_JSON_3/index.js
@@ -1,7 +1,6 @@
-//Programa lector JSON 3 asincronico
+'use strict';
 const fs = require('fs');
-const rawdata = fs.readFileSync('C:/Users/avanc/Avancini_Macrina/Modulo05/TP_07/personas.json','utf8');
-var persona = JSON.parse(rawdata);
-for(var i=0;i<persona['personas'].length;i++){
-console.log(persona['personas'][i]['nombre']);
-}
+fs.readFile('C:/Users/avanc/Avancini_Macrina/Modulo05/TP_07/personas.json', (err, data) => {
+if (err) throw err;
+console.log(JSON.parse(data));
+});
