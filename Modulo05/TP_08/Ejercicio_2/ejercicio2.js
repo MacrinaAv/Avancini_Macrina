9e18eb7be38e595fcf20473936f46b6abe2b2fbb
MacrinaAv <avanciniluz@gmail.com>
2022-11-29 09:38:34 -0300

    ejercicio 2 del tp_8

---
+++ Modulo05/TP_08/Ejercicio_2/ejercicio2.js
@@ -0,0 +1,7 @@
+//
+'use strict';
+let jsonData = require('C:/Users/avanc/Avancini_Macrina/Modulo05/TP_07/personas.json');
+console.log(JSON.stringify(jsonData)); 
+//a) en este caso JSON.stringify() lo combierte nuevamente en JSON
+//b)
+console.log(JSON.parse(jsonData));
