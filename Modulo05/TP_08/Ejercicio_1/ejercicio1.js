52df848bc885cfd52502f46e91136fb81d2c9d53
MacrinaAv <avanciniluz@gmail.com>
2022-11-29 12:29:02 -0300

    corrección ejercicio 1

--- Modulo05/TP_08/Ejercicio_1/ejercicio1.js
+++ Modulo05/TP_08/Ejercicio_1/ejercicio1.js
@@ -2,5 +2,5 @@
 const fs = require('fs');
 let rawdata = fs.readFileSync('C:/Users/avanc/Avancini_Macrina/Modulo05/TP_07/personas.json');
 console.log(JSON.stringify(rawdata));
-//JSON.parce() toma un archivo lo interpreta como JSON y lo traduce a JavaScript (lo estructutra)
-//JSON.stringify() convierte a JSON .. como en este caso el archivo ya es JSON lo hace mal
+//JSON.parce() toma un archivo lo interpreta como JSON y lo traduce a JavaScript (lo estructutra, procesa)
+//JSON.stringify() convierte a JSON .. como en este caso el archivo ya es JSON lo puede procesar.. aparecen los datos no procesados
