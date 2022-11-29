ddfd782d60fdcf456cfc0e249de583f12c6fa695
MacrinaAv <avanciniluz@gmail.com>
2022-11-29 10:19:12 -0300

    correcciones

--- Modulo05/TP_08/Ejercicio_1/ejercicio1.js
+++ Modulo05/TP_08/Ejercicio_1/ejercicio1.js
@@ -2,5 +2,5 @@
 const fs = require('fs');
 let rawdata = fs.readFileSync('C:/Users/avanc/Avancini_Macrina/Modulo05/TP_07/personas.json');
 console.log(JSON.stringify(rawdata));
-// JSON.parce() toma un archivo JSON y lo traduce a JavaScript (lo estructutra)
-//JSON.stringify() traduce a JSON .. como en este caso el archivo ya es JSON lo hace mal
+//JSON.parce() toma un archivo lo interpreta como JSON y lo traduce a JavaScript (lo estructutra)
+//JSON.stringify() convierte a JSON .. como en este caso el archivo ya es JSON lo hace mal
--- Modulo05/TP_08/Ejercicio_2/ejercicio2.js
+++ Modulo05/TP_08/Ejercicio_2/ejercicio2.js
@@ -3,5 +3,6 @@
 let jsonData = require('C:/Users/avanc/Avancini_Macrina/Modulo05/TP_07/personas.json');
 console.log(JSON.stringify(jsonData)); 
 //a) en este caso JSON.stringify() lo combierte nuevamente en JSON
-//b)
+
 console.log(JSON.parse(jsonData));
+//b) como jsonData ya esta en formato JavaScript no lo reconoce
